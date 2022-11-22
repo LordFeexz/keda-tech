fa81a9cd01eef208943fb5beaa1e4529f3d3c1ae
LordFeexz <anandafiqri1403@gmail.com>
2022-11-22 12:22:31 +0700

    vehicle data init

---
+++ migrations/20221122050856-create-vehicle.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Vehicles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      plat: {
+        type: Sequelize.STRING
+      },
+      checkinDate: {
+        type: Sequelize.DATE
+      },
+      checkoutDate: {
+        type: Sequelize.DATE
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Vehicles');
+  }
+};
